7c87f79d38c8cd6ddbd7ffe8aaf55ab22b7efc96
Edmund chend <Edmund.Chen@activenetwork.com>
2019-02-22 15:05:59 +0800

    chore: modify toast demo button size

--- src/toast/demo/index.js
+++ src/toast/demo/index.js
@@ -32,21 +32,21 @@ function loadingToast() {
 export default () => (
   <div>
     <DemoBlock title="基本文字提示" className="has-padding">
-      <Button inline onClick={showToast} style={{ marginRight: 20 }}>文字提示</Button>
-      <Button inline onClick={showToastNoMask}>无mask</Button>
+      <Button size="small" inline onClick={showToast} style={{ marginRight: 20 }}>文字提示</Button>
+      <Button size="small" inline onClick={showToastNoMask}>无mask</Button>
     </DemoBlock>
 
     <DemoBlock title="成功/失败" className="has-padding">
-      <Button inline onClick={successToast} style={{ marginRight: 20 }}>成功提示</Button>
-      <Button inline onClick={failToast}>失败提示</Button>
+      <Button size="small" inline onClick={successToast} style={{ marginRight: 20 }}>成功提示</Button>
+      <Button size="small" inline onClick={failToast}>失败提示</Button>
     </DemoBlock>
 
     <DemoBlock title="网络离线状态" className="has-padding">
-      <Button onClick={offline}>网络离线状态</Button>
+      <Button size="small" onClick={offline}>网络离线状态</Button>
     </DemoBlock>
 
     <DemoBlock title="loading" className="has-padding">
-      <Button onClick={loadingToast}>loading</Button>
+      <Button size="small" onClick={loadingToast}>loading</Button>
     </DemoBlock>
   </div>
 );
