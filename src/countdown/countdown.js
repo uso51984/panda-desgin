9352dac37b7cb4b743735de6ea968e1c945dfcc3
EdmundChen <usocjb@163.com>
2019-03-19 21:15:00 +0800

    feat: countdown components

--- src/countdown/countdown.js
+++ src/countdown/countdown.js
@@ -3,26 +3,27 @@ import { restTimeFunc } from './utils';
 
 export default class Countdown extends React.PureComponent {
   static defaultProps = {
-    paused: true,
+    paused: false,
     showDays: true,
     showPlainText: false,
     interval: 1000,
     startTime: Date.now(),
     endTime: Date.now(),
+    onChange() {},
   }
 
   constructor(props) {
     super(props);
     this.state = {
       restTime: this.formatTime(0),
-      p: 0,
-      _curr: 0,
     };
+
+    this.p = 0;
+    this._curr = 0;
   }
 
   componentDidMount() {
-    const { interval, startTime, endTime, paused, onChange } = this.props;
-    const { p } = this.state;
+    const { interval, startTime, endTime, onChange } = this.props;
     const currunt = Date.now();
     const start = new Date(startTime).getTime();
     const end = new Date(endTime).getTime();
@@ -31,8 +32,8 @@ export default class Countdown extends React.PureComponent {
     this.setState({ restTime: this.formatTime(end - (start + diffTime)) });
 
     this.timer = setInterval(() => {
-      if (paused) {
-        let restTime = end - (new Date().getTime() - p + diffTime);
+      if (!this.props.paused) {
+        let restTime = end - (new Date().getTime() - this.p + diffTime);
         restTime -= 1000;
         this.setState({ restTime: this.formatTime(restTime) }, () => {
           onChange(this.formatTime(restTime), restTime);
@@ -48,8 +49,16 @@ export default class Countdown extends React.PureComponent {
     }, interval);
   }
 
+  componentWillReceiveProps() {
+    if (!this.props.paused) {
+      this._curr = Date.now();
+    } else {
+      this.p += (Date.now() - this._curr);
+    }
+  }
+
   getPlainText() {
-    const { d, h, m, s } = this.state.resttime;
+    const { d, h, m, s } = this.state.restTime;
     const { showDays } = this.props;
 
     return `${d > 0 && showDays ? `${d}天${h}` : h}小时${m}分${s}秒`;
@@ -67,13 +76,11 @@ export default class Countdown extends React.PureComponent {
     return rest;
   }
 
-  render() {
-    const { showPlainText, showDays } = this.props;
+  renderDefaultTime() {
     const { restTime } = this.state;
-
+    const { showDays } = this.props;
     return (
-      <div>
-        {showPlainText && <span className="panda-cd-timer">{this.getPlainText()}</span>}
+      <React.Fragment>
         {restTime.d > 0 && showDays && [
           <span className="panda-cd-block" key="day">{restTime.d}</span>,
           <span className="panda-cd-dot" key="day-label">天</span>,
@@ -83,6 +90,19 @@ export default class Countdown extends React.PureComponent {
         <span className="panda-cd-block">{restTime.m}</span>
         <span className="panda-cd-dot">:</span>
         <span className="panda-cd-block">{restTime.s}</span>
+      </React.Fragment>
+    );
+  }
+
+  render() {
+    const { showPlainText } = this.props;
+    return (
+      <div>
+        {showPlainText ?
+          <span className="panda-cd-timer">{this.getPlainText()}</span>
+          :
+          this.renderDefaultTime()
+        }
       </div>
     );
   }
--- src/countdown/demo/index.js
+++ src/countdown/demo/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import DemoBlock from 'docs/mobileComponents/DemoBlock';
 import Countdown from '../index';
+import Button from 'src/button';
 
-export default () => (
-  <div>
-    <DemoBlock title="基础用法">
-      <Countdown endTime={1559334689373} onChange={(timeObj) => {
-        console.log('timeObj', timeObj);
-      }} />
-    </DemoBlock>
-  </div>
-);
+export default class Demo extends React.PureComponent {
+  state = {
+    paused: false,
+  }
+
+  render() {
+    return (
+      <div>
+        <DemoBlock title="基础用法">
+          <Countdown endTime={1559334689373} />
+        </DemoBlock>
+        <DemoBlock title="基础用法">
+          <Countdown endTime={1559334689373} showPlainText paused={this.state.paused} />
+          <Button onClick={() => this.setState({ paused: !this.state.paused })}>暂停</Button>
+        </DemoBlock>
+      </div>
+    );
+  }
+}
