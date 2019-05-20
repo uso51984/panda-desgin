eae20214b6d8d6530d69fc0e0723914858a2e67c
Edmund chend <Edmund.Chen@activenetwork.com>
2019-05-20 17:21:29 +0800

    chore: 优化countdown demo

--- src/countdown/demo/index.js
+++ src/countdown/demo/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import DemoBlock from 'docs/mobileComponents/DemoBlock';
-import Countdown from '../index';
 import Button from 'src/button';
+import Countdown from '../index';
+import './index.less';
 
 export default class Demo extends React.PureComponent {
   state = {
@@ -12,14 +13,30 @@ export default class Demo extends React.PureComponent {
   render() {
     const { paused, showDays } = this.state;
     return (
-      <div>
+      <div className="count-down-demo">
         <DemoBlock title="基础用法">
-          <Countdown endTime={1559334689373} showDays={showDays} />
+          <div className="base-count-down">
+            <Countdown endTime={1559334689373} />
+          </div>
         </DemoBlock>
-        <Button onClick={() => this.setState({ showDays: !showDays })}>{`${showDays ? 'hide day' : 'show day'}`}</Button>
-        <DemoBlock title="基础用法">
-          <Countdown endTime={1559334689373} showPlainText paused={paused} />
-          <Button onClick={() => this.setState({ paused: !paused })}>暂停</Button>
+
+        <DemoBlock title="显示天">
+          <div className="base-count-down">
+            <Countdown endTime={1559334689373} showDays />
+          </div>
+        </DemoBlock>
+
+        <DemoBlock title="显示时分秒">
+          <div className="base-count-down">
+            <Countdown endTime={1559334689373} showPlainText />
+          </div>
+        </DemoBlock>
+
+        <DemoBlock title="可暂停">
+          <div className="base-count-down">
+            <Countdown endTime={1559334689373} showPlainText paused={paused} />
+          </div>
+          <Button onClick={() => this.setState({ paused: !paused })}>{this.state.paused ? '开始' : '暂停'}</Button>
         </DemoBlock>
       </div>
     );
