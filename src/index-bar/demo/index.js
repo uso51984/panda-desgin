dea2a0ac95d00ddf083f72d349ded039c806772b
EdmundChen <usocjb@163.com>
2019-08-11 21:56:39 +0800

    feat: indexBar

--- src/index-bar/demo/index.js
+++ src/index-bar/demo/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import DemoBlock from 'docs/mobileComponents/DemoBlock';
 import Button from 'src/button';
+import { Cell } from '../../cell';
 import IndexAnchor from '../indexAnchor';
 import IndexBar from '../index';
 import './index.less';
@@ -23,24 +24,17 @@ export default class Demo extends React.PureComponent {
   }
 
   render() {
-    const { paused, showDays } = this.state;
     return (
-      <div className="count-down-demo">
-        <DemoBlock title="基础用法">
-          <div className="base-count-down">
-            <IndexBar>
-              {
-                indexList.map((text, index) => (
-                  <IndexAnchor text={text} key={`${text}-${index}`}>
-                    {dataTwo.map(() => (<div>adfasdf32232</div>))}
-                  </IndexAnchor>
-                ))
-              }
-            </IndexBar>
-          </div>
-        </DemoBlock>
-
-
+      <div className="index-bar-demo">
+        <IndexBar>
+          {
+            indexList.map((text, index) => (
+              <IndexAnchor text={text} key={`${text}-${index}`}>
+                {dataTwo.map(() => (<Cell title="单元格" />))}
+              </IndexAnchor>
+            ))
+          }
+        </IndexBar>
       </div>
     );
   }
